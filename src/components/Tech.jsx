7e57eb7246e0cbaf9572df033376567865313336
Handemore7 <handemore7@gmail.com>
2023-08-15 11:38:02 -0500

    click test ga4

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -6,8 +6,8 @@ import { technologies } from '../constants'
 const Tech = () => {
   return (
     <div className='flex flex-row flex-wrap justify-center gap-10'>
-      {technologies.map((technology, index) => (
-        <div key={technology.name} className='w-28 h-28'>
+      {technologies.map((technology) => (
+        <div key={technology.name} id={technology.name} className='w-28 h-28'>
           <BallCanvas icon={technology.icon}/>
         </div>
       ))}
